2a21d6a4e8435a1e10644d288fea5e7f3593eeaf
sss193 <sunny4get@gmail.com>
2022-10-14 01:33:26 +0530

    Temp Solution

--- app/src/main/java/com/vicky/halodoctest/persentation/GridAdapter.kt
+++ app/src/main/java/com/vicky/halodoctest/persentation/GridAdapter.kt
@@ -21,6 +21,7 @@ class GridAdapter(private val context: Context, private val list: ArrayList<Imag
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
 
 
+
             val convertView = LayoutInflater.from(context).inflate(R.layout.image_row_layout, null)
             val imageview: ImageView = convertView.findViewById(R.id.imageView) as ImageView
             val textview = convertView.findViewById(R.id.textView) as TextView
@@ -50,6 +51,25 @@ class GridAdapter(private val context: Context, private val list: ArrayList<Imag
         override fun getCount(): Int = list.size
 
     fun addData(imageResultModels: List<ImageResultModel>) {
-        list.addAll(imageResultModels.take(75))
+        list.clear()
+        var reverse = false
+
+        for( i in 0..74 step 4){
+            if(reverse){
+                list.add(imageResultModels[i+3])
+                list.add(imageResultModels[i+2])
+                list.add(imageResultModels[i+1])
+                list.add(imageResultModels[i])
+            }else{
+                list.add(imageResultModels[i])
+                list.add(imageResultModels[i+1])
+                list.add(imageResultModels[i+2])
+                list.add(imageResultModels[i+3])
+            }
+            reverse = !reverse
+        }
+
+        // Temp solution this can done using gridLayout manager
+        //list.addAll(imageResultModels.take(75)
     }
     }
